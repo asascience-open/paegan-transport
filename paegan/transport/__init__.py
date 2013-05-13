dd9857c11652cf32139256ff6b44a96d16b2f3a7
Kyle Wilcox <kwilcox@asascience.com>
2013-05-13 17:55:03 -0400

    Inital commit of the transport module

---
+++ paegan/transport/__init__.py
@@ -0,0 +1,12 @@
+import logging
+
+try:
+    PROGRESS=15
+    logging.PROGRESS = PROGRESS
+    logging.addLevelName(PROGRESS, 'PROGRESS')
+    def progress(self, message, *args, **kws):
+        if self.isEnabledFor(PROGRESS):
+            self._log(PROGRESS, message, args, **kws)
+    logging.Logger.progress = progress
+except:
+    pass
---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages, Command
+
+files = ["paegan/*"]
+readme = open('README.md', 'rb').read()
+reqs = [line.strip() for line in open('requirements.txt')]
+
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import sys,subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+setup(namespace_packages = ['paegan'],
+    name                = "paegan-transport",
+    version             = "0.1.0",
+    description         = "Particle transport packages for the Paegan library",
+    long_description    = readme,
+    license             = 'LICENSE.txt',
+    author              = "Kyle Wilcox",
+    author_email        = "kwilcox@sasascience.com",
+    url                 = "https://github.com/asascience-open/paegan-transport",
+    packages            = find_packages(),
+    cmdclass            = {'test': PyTest},
+    install_requires    = [
+                            "GDAL == 1.9.1",
+                            "Fiona == 0.8",
+                            "paegan >= 0.9.3"
+                          ],
+    classifiers         = [
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering',
+        ],
+    include_package_data = True,
+) 
